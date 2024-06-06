3e322ccf77771f26c904dc3a9fc611fab6cd14c9
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-06-07 03:41:15 +0600

    default db.sqlite3

--- aaproject/aaproject/settings.py
+++ aaproject/aaproject/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = 'aaproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydb1",
-        "USER": "postgres",
-        "PASSWORD": "1902171",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
